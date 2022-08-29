4423b9ed7647d7a1a9b757b9385f2ec09ae82f64
jarednjk <jared.ng@trentglobal.edu.sg>
2022-08-29 08:32:47 +0000

    add search filter to api products

--- routes/api/products.js
+++ routes/api/products.js
@@ -22,7 +22,88 @@ router.get('/:product_id/variants', async (req, res) => {
     }
 })
 
-router.post('/', async (req, res) => {
+router.get('/materials', async (req, res) => {
+    try {
+        const materials = await productDataLayer.getAllMaterials()
+        materials.unshift([0, '--SELECT MATERIAL--']);
+        res.send(materials)
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
+router.get('/categories', async (req, res) => {
+    try {
+        const categories = await productDataLayer.getAllCategories()
+        categories.unshift([0, '--SELECT CATEGORY--']);
+        res.send(categories)
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
+router.get('/brands', async (req, res) => {
+    try {
+        const brands = await productDataLayer.getAllBrands()
+        brands.unshift([0, '--SELECT BRAND--']);
+        res.send(brands)
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
+router.get('/features', async (req, res) => {
+    try {
+        const features = await productDataLayer.getAllFeatures()
+        res.send(features)
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
+router.post('/search', async (req, res) => {
+    const q = Product.collection();
+
+    if (Object.keys(req.body).length === 0) {
+        const products = await q.fetch({
+            withRelated: ['material', 'brand', 'category', 'features']
+        })
+        res.send(products)
+    }
+    else if (Object.keys(req.body).length != 0) {
+        if (req.body.name) {
+            q.where('name', 'ilike', '%' + req.body.name + '%')
+        }
+        if (req.body.min_cost) {
+            q.where('cost', '>=', req.body.min_cost)
+        }
+        if (req.body.max_cost) {
+            q.where('cost', '<=', req.body.max_cost)
+        }
+        if (req.body.min_card_slot) {
+            q.where('weight', '>=', req.body.min_card_slot)
+        }
+        if (req.body.max_card_slot) {
+            q.where('weight', '<=', req.body.max_card_slot)
+        }
+        if (req.body.material_id) {
+            q.where('material_id', '=', req.body.material_id)
+        }
+        if (req.body.category_id) {
+            q.where('category_id', '=', req.body.category_id)
+        }
+        if (req.body.brand_id) {
+            q.where('brand_id', '=', req.body.brand_id)
+        }
+        if (req.body.features) {
+            q.query('join', 'features_products', 'products.id', 'product_id')
+            .where('feature_id', 'in', form.data.features.split(','))
+        }
+        const products = await q.fetch({
+            withRelated: ['material', 'brand', 'category', 'features']
+        })
+        res.send(products);
+    }
     
 })
 
